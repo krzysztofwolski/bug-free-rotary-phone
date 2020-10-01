bcdc6db9e034bf34b2cc38a77ad5c7dc20673e40
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2020-10-01 12:00:55 +0200

    Remove unused title

--- src/components/templates/HomepageTemplate/HomepageTemplate.tsx
+++ src/components/templates/HomepageTemplate/HomepageTemplate.tsx
@@ -1,4 +1,3 @@
-import { Box, Text } from '@chakra-ui/core'
 import React from 'react'
 import { ICollectionFragment } from '../../../gql/generated/interfaces'
 import { ProductList } from '../../organisms'
@@ -10,13 +9,9 @@ export interface HomepageTemplateProps {
 
 export const HomepageTemplate: React.FC<HomepageTemplateProps> = ({
   homepageCollection,
-  shopName,
 }) => {
   return (
     <>
-      <Box>
-        <Text fontSize="3xl">`Welcome to ${shopName}!`</Text>
-      </Box>
       {!!homepageCollection && (
         <ProductList
           products={
